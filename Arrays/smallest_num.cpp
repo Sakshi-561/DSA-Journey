28cdb9b69784316ed5223fae494af6b5b1c05392
Sakshi Tiwari <tiwarisakshi20005@gmail.com>
2026-08-22 16:25:47 +0530

    Added smallest number problem and README

---
+++ Arrays/smallest_num.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter the size of array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int smallest = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] < smallest) {
+            smallest = arr[i];
+        }
+    }
+
+    cout << "Smallest number = " << smallest << endl;
+
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,23 @@
+#  DSA Journey
+
+Welcome to my DSA Journey! 
+
+I am learning Data Structures and Algorithms using C++ and solving problems step by step to improve my problem-solving and coding skills.
+
+## Topics
+
+### Arrays
+- Smallest Number in an Array
+
+More problems coming soon...
+
+##  Language
+- C++
+
+##  Goal
+To build strong DSA fundamentals and improve my problem-solving skills.
+
+> Learning → Practicing → Improving 
+
+## Author
+Sakshi Tiwari
