8252a488ce192b0f2f3297ceb46f9fc19b195ec8
Sakshi Tiwari <tiwarisakshi20005@gmail.com>
2026-09-17 20:14:38 +0530

    Solved pop at back

---
+++ Linked -List/Pop_back.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+using namespace std;
+
+class node{
+public:
+    int data;
+    node* next;
+
+    node(int val){
+        data=val;
+        next=NULL;
+    }
+
+};
+ class List{
+    node* head;
+    node* tail;
+public:
+    List(){
+        head=tail=NULL;
+    }
+    void push_back(int val){
+        node* newNode=new node(val);
+        if(head==NULL){
+            head=tail=newNode;
+        }
+        else{
+            tail->next=newNode;
+            tail=newNode;
+        }
+    }
+
+    void pop_back(){
+    if(head==NULL){
+        cout<<"Linked list is empty\n";
+        return;
+    }
+
+    node* temp=head;
+
+    while(temp->next != tail){
+        temp=temp->next;
+    }
+
+    temp->next=NULL;
+    delete tail;
+    tail=temp;
+}
+
+    void print(){
+        node* temp=head;
+        while(temp!=NULL){
+            cout<<temp->data<<"->";
+            temp=temp->next;
+        }
+        cout<<"NULL"<<endl;
+    }
+};
+int main(){
+    List ll;
+    ll.push_back(4);
+    ll.push_back(3);
+    ll.push_back(2);
+    ll.push_back(1);
+
+    ll.print(); 
+
+    ll.pop_back();
+    
+    ll.print(); 
+    return 0;
+}
+ 
